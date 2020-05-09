34fbf3cc9cd48ebe8a45db55f1b9ed7c973087b6
Madison Keene <madikeene93@gmail.com>
2020-05-09 18:58:37 +0100

    Fix error

--- horseracing/__init__.py
+++ horseracing/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask, g, redirect, url_for, render_template
-from horseracing.db import init_db
 import os
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    init_db()
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'tmp.sqlite'),
