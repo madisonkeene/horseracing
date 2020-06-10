296ac0c4b7e114da8a9b8db90afe8f6a34906df6
Madison Keene <madikeene93@gmail.com>
2020-06-10 21:20:34 +0100

    Move secret key to environment var

--- horseracing/__init__.py
+++ horseracing/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.environ['SECRET_KEY'],
     )
 
     if test_config is None:
